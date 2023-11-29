7794d88a0b4fcec3b65ec33cc0f0437d19ff15ff
abdur-rehman-ali <alijoyia911@gmail.com>
2023-11-28 16:22:24 -0800

    Register custom model in admin file

--- account/admin.py
+++ account/admin.py
@@ -1,3 +1,32 @@
+#Django Imports
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
 
-# Register your models here.
+#Local Imports
+from account.models import User
+
+class UserAdmin(BaseUserAdmin):
+  list_display = ["id", "name", "email", "is_admin"]
+  list_filter = ["is_admin"]
+  fieldsets = [
+      ("Credentials", {"fields": ["email", "password"]}),
+      ("Personal info", {"fields": ["name"]}),
+      ("Permissions", {"fields": ["is_admin"]}),
+  ]
+  add_fieldsets = [
+    (
+      None,
+      {
+        "classes": ["wide"],
+        "fields": ["email", "name", "password1", "password2"],
+      },
+    ),
+  ]
+  search_fields = ["id", "name", "email"]
+  ordering = ["id", "name", "email"]
+  filter_horizontal = []
+
+
+
+admin.site.register(User, UserAdmin)
+  
--- account/models.py
+++ account/models.py
@@ -3,7 +3,7 @@ from django.db import models
 from django.contrib.auth.models import AbstractBaseUser
 
 #Local Imports
-from .managers import UserManager
+from account.managers import UserManager
 
 class User(AbstractBaseUser):
   email = models.EmailField(
